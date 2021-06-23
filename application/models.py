6d06aa3d54d5fdf73562358dfff227b6912c6712
Samuel <Sam.castle02@gmail.com>
2021-06-23 15:04:28 +0000

    create table test

--- application/models.py
+++ application/models.py
@@ -4,19 +4,20 @@ class Store(db.Model):
     store_id = db.Column(db.Integer, primary_key=True)
     shop_name = db.Column(db.String(50))
     shop_address = db.Column(db.String(100))
+    shop_postcode = db.Column(db.String(30))
     distance_to_travel = db.Column(db.Float)
     takeaway = db.Column(db.Boolean)
-    reciepts = db.relationship('Store', backref= 'store_id')
+    reciepts = db.relationship('Receipts', backref= 'Store')
     
 class Receipts(db.Model):
     receipt_id = db.Column(db.Integer, primary_key=True)
-    Most_expensive = db.Column(db.Integer)
-    cost_of_alcohol = db.Column(db.Integer)
+    most_expensive = db.Column(db.Float)
+    cost_of_alcohol = db.Column(db.Float)
     date_of_reciept = db.Column(db.DateTime)
-    reciept_total = db.Column(db.Integer)
+    receipt_total = db.Column(db.Float)
     takeaway = db.Column(db.Boolean)
-    delivery_fee = db.Column(db.Integer)
-    delivery_time = db.Column(db.DateTime)
+    delivery_fee = db.Column(db.Float)
+    delivery_time_mins = db.Column(db.Integer)
     store_id = db.Column(db.Integer, db.ForeignKey('store.store_id'), nullable=False)
     
 
--- create.py
+++ create.py
@@ -4,5 +4,8 @@ from application.models import Store, Receipts
 db.drop_all()
 db.create_all()
 
+teststore = Store(shop_name='McDonalds', shop_address='63 Northbrook st', shop_postcode='RG14 1AE', takeaway=True)
+testreceipt = Receipts(most_expensive=5.09, cost_of_alcohol=0, date_of_reciept=2021-6-23, receipt_total=11.36, takeaway=True, delivery_fee=1.99, delivery_time_mins=28)
 
-
+db.session.add(teststore, testreceipt)
+db.session.commit()
